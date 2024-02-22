3db721c99e89adfb7715ec311ff8e75cc8cda19d
Kirill_divine <kirill.sokolov96@gmail.com>
2024-02-22 17:34:07 +0200

    Added a way to read information from the console

--- src/main/java/basic/syntax/Main.java
+++ src/main/java/basic/syntax/Main.java
@@ -1,14 +1,24 @@
 package basic.syntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int adultHood = 18;
         int userAge;
+        String userName = "";
         Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Hello, how old are you?");
+        System.out.println("Hello, what is your name?");
+        userName = reader.readLine();
+        System.out.println("What date were you born? (just write a number)");
+        int date = Integer.parseInt(reader.readLine());
+
+        System.out.println("Hello, " + userName + ", how old are you?");
         userAge = scanner.nextInt();
 
         if (userAge >= adultHood){
