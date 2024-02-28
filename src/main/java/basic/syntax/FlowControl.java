6e5ada363a510879db9c339189db0116e1384133
Kirill_divine <kirill.sokolov96@gmail.com>
2024-02-28 18:35:28 +0200

    added an exaple of switch-case statement

--- src/main/java/basic/syntax/FlowControl.java
+++ src/main/java/basic/syntax/FlowControl.java
@@ -1,18 +1,31 @@
 package basic.syntax;
 
 public class FlowControl {
-    public static void  main(String[] args){
+    public static void main(String[] args) {
     }
 
-    public  static  void  ifElseDemo(boolean var){
-        if (var){
+    public static void ifElseDemo(boolean var) {
+        if (var) {
             System.out.println(1);
-        }
-        else if (!var){
+        } else if (!var) {
             System.out.println(0);
-        }
-        else {
+        } else {
             System.out.println();
         }
+    }
+
+    public static int switchCaseDemo(int userInput) {
+        switch (userInput) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                break;
+            default:
+                return -1;
+        }
+
+        return 2;
     }
 }
