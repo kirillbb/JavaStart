0681a481a1b825d2a33a029e736fd706b0df12da
Kirill_divine <kirill.sokolov96@gmail.com>
2024-02-28 18:56:51 +0200

    added an exaple of for loop

--- src/main/java/basic/syntax/FlowControl.java
+++ src/main/java/basic/syntax/FlowControl.java
@@ -41,4 +41,12 @@ public class FlowControl {
             }
         }
     }
+
+    public static void forDemo() {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
