d9a0613c04b6fd6c7993e2171fc839079af556ed
Kirill_divine <kirill.sokolov96@gmail.com>
2024-02-28 18:27:34 +0200

    added an exaple of if-else statement

---
+++ src/main/java/basic/syntax/FlowControl.java
@@ -0,0 +1,17 @@
+package basic.syntax;
+
+public class FlowControl {
+    public static void  main(String[] args){
+    }
+
+    public  static  void  ifElseDemo(boolean var){
+        if (var){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+
+    
+}
